c62cfe9a74ff6bca0fd207aa41eb3b4a9e5ceac8
tmuq <70723095+tmuq@users.noreply.github.com>
2021-05-25 11:42:21 -0700

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Dane Hobrecht
+/* Copyright 2021 tmuq
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
